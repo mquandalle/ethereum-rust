5cc1f651be8c105826dfde458aad1b9939bbb1a4
Maxime Quandalle <maxime.quandalle@gmail.com>
2014-06-15 11:40:24 +0200

    [WIP] Improve trie

--- trie.rs
+++ trie.rs
@@ -1,78 +1,112 @@
 // A modified Radix+Merkle tree to encode the state of all contracts
 #![feature(struct_variant)]
 
-use db::DB;
+use lib::leveldb::database;
 
-struct Key(Vec<u8>);
-struct Value(Vec<u8>);
-struct Ref(Vec<u8>);
-
-impl Ref {
-  fn start_with(&self, search: Vec<u8>) -> bool {
-    false
-  }
-  fn get_node(&self, db: &DB) -> Node {
-    Blank
-  }
-}
+type Key   = Vec<u8>;
+type Value = Vec<u8>;
+type Ref   = Vec<u8>;
 
 enum Node {
   Blank,
-  Leaf{ key: Key, val: Value },
-  Extension{ key: Key, val: Ref },
-  Branch{ refs: [Ref, ..16], val: Value },
+  Leaf{ key: Key, value: Value },
+  Extension{ key: Key, value: Ref },
+  Branch{ refs: [Ref, ..16], value: Value },
 }
 
 impl Node {
   fn isTerminal(&self) -> bool {
     match *self {
-      Blank | Leaf{..} => { true },
-      Extension{..} | Branch{..} => { false }
+      Blank | Leaf{..} => true,
+      Extension{..} | Branch{..} => false
     }
   }
 }
 
 struct Trie {
-  database: DB,
-  root_hash: Ref
+  database: database,
+  root_id: Ref
 }
 
 impl Trie {
+  pub fn new(database: database, root_id: Ref) -> Trie {
+    Trie {
+      database: database,
+      root_id: root_id
+    }
+  }
+
   pub fn get(&self, key: Key) -> Value {
-    Trie::get_from_node(self.root_hash.get_node(), key)
+    self.get_from_node(self.root_id, key)
   }
 
-  pub fn set(&self, key: Key, val: Value) {
-
+  pub fn set(&self, key: Key, value: Value) {
+    self.set_from_node(self.root_id, key, value)
   }
 
-  pub fn update(&self, key: Key, new_val: Value) {
-
+  pub fn remove(&self, key: Key) {
+    self.set_from_node(self.root_id, key, Value(vec![]))
   }
 
-  pub fn delete(&self, key: Key) {
 
+
+  fn get_node(&self, node_id: Ref) -> Node {
+    self.database.get(node_id.unwrap())
   }
 
-  fn get_from_node(db: &DB, node: &Node, search: Key) -> Value {
-    let node = node_id.get_node();
-    match *node {
-      Leaf{key, val} if key == search => {
-        val
+  fn get_from_node(&self, node_id: Ref, search: Key) -> Value {
+    let node = self.get_node(node_id);
+    let null_vec = vec![0u8, ..32];
+    match node {
+      Blank => null_vec,
+
+      Leaf{key, value} => {
+        if key == search { value }
+        else { null_vec }
       },
-      Extension{key, val} if key.start_with(search) {
-        Trie::get_from_node(val.get_node(db))
+
+      Extension{key, value} => {
+        // XXX
+        self.get_from_node(value, search.split())
       }
-      Branch{refs, val} {
-        // TODO
-      }
-      _ => {
-        vec![0u8, ..32]
+
+      Branch{refs, value} => {
+        if search.len() == 0 {
+          node.value
+        } else {
+          let child_id = refs[search[0]];
+          self.get_from_node(child_id, search.slice_from(1).to_vec())
+        }
       }
     }
   }
-}
 
-fn main() {
+  fn set_from_node(&self, node_id: Ref, key: Key, value: Value) -> Node {
+    let node = self.get_node(node_id);
+    match node {
+      Blank => {
+        [pack_nibbles(with_terminator(key)), value.unwrap()]
+      },
 
+      Leaf => {
+
+      },
+
+      Extension => {
+
+      },
+
+      Branch => {
+        if key == "" {
+          node.value = value
+        } else {
+          let child = self.update_and_delete_storage()
+        }
+      }
+    }
+  }
+
+  fn update_and_delete_storage(&self, node: Node, key: Key, value: Value) {
+
+  }
 }
